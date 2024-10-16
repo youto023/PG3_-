d38ebf417b89cd56e6a1986fc131e6567b88a1d9
youto023 <yuuto9036@gmail.com>
2024-10-16 16:32:44 +0900

    1_2課題完成

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
-#include <stdio.h>
-#include<Windows.h>
-int main() {
+#include<stdio.h>
+int Recursive(int n) {
+    if (n <= 1) {
+        return 100;
+    }
+    return (Recursive(n - 1) * 2) - 50;
+}
 
-	printf("ああああ");
-	return 0;
+int main() {
+    int n = 10;
+    int wages = 1072;
+    int result;
+
+    for (int i = 1; i < 11; i++) {
+        result = Recursive(i);
+        printf("一般的な時給 : %d\n", wages);
+        printf("再帰的な給料 : %d\n\n", result);
+        wages += 1072;
+    }
+    return(0);
 }
