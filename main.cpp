7682adcefe77c02527902d034e7debf78db185c0
youto023 <yuuto9036@gmail.com>
2025-01-28 10:38:07 +0900

    06_02完成

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
-#include <stdio.h>
-#include<Windows.h>
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+void PrintThread(uint32_t num) {
+	cout << "thread No" << num << endl;
+}
+
 int main() {
 
-	printf("ああああ");
+	thread t1(PrintThread, 1);
+	t1.join();
+	thread t2(PrintThread, 2);
+	t2.join();
+	thread t3(PrintThread, 3);
+	t3.join();
+
 	return 0;
 }
