66d00eff928d1bf5b538a0976f596a1a0a7545d8
youto023 <yuuto9036@gmail.com>
2025-02-03 14:23:10 +0900

    07_01完成

--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
 #include <stdio.h>
-#include<Windows.h>
+#include <string>
+
 int main() {
 
-	printf("ああああ");
-	return 0;
+
+    std::string a(1000000, 'a');
+    std::string aCopy;
+    std::string aMove;
+
+    // 開始時刻を取得
+    auto startCopy = std::chrono::high_resolution_clock::now();
+
+
+    aCopy = a;
+
+
+    // 終了時刻を取得
+    auto endCopy = std::chrono::high_resolution_clock::now();
+
+    // 経過時間をマイクロ秒単位で計算
+    auto durationCopy = std::chrono::duration_cast<std::chrono::microseconds>(endCopy - startCopy);
+
+    // 結果を表示
+    std::cout << "コピー時間: " << durationCopy.count() << " μs\n";
+
+    return 0;
 }
